5da8bac2f1a99a96baac2e2c41c60ff60e75476c
startshineye <13001955858@163.com>
2018-06-10 13:45:27 +0800

    添加DataContext

---
+++ vkefu/src/main/java/com/vkefu/core/DataContext.java
@@ -0,0 +1,91 @@
+package com.vkefu.core;
+public class DataContext{
+	/**
+	 * 排队中的坐席是否已被通知过
+	 */
+	public enum IsUserWaitNotify{
+		NOTIFY,
+		UNNOTIFY;
+		public String toString(){
+			return super.toString().toLowerCase() ;
+		}
+	}
+	/**
+	 *分配坐席结果状态
+	 */
+	public enum AllotAgentStatusEnum{
+		INSERVICE,
+		INQUENE,NOAGENT,TIMEOUT;
+		public String toString(){
+			return super.toString().toLowerCase() ;
+		}
+	}
+	/**
+	 *微信响应消息类型
+	 */
+	public enum WexinRespMessageTypeEnum{
+		TEXT,IMAGE,VOICE,VIDEO;
+		public String toString(){
+			return super.toString().toLowerCase() ;
+		}
+	}
+	/**
+	 * 命令
+	 */
+	public enum CmdTypeEnum{
+		 AGENTSTATEHEART,//状态坐席心跳
+		 AGENTHEART,//坐席心态
+		 USERHEART,//用户心跳
+		 WEBCCSTART,//web用户发送信息
+		 WEBCHATCLIENTMSG,//客户发送web聊天信息(C-->A)
+		 WEBCHATCLIENTEND,//用户超时结束聊天
+		 WEBCHATAGENTMSG,//坐席发送web聊天信息(A-->C)
+		 AGENTSTATUS,//坐席报告状态
+		 FOCUSAGENTLOGIN,//坐席开始报告状态,用于绑定特定的坐席状态socketSession,focus中agent登录
+		 WEBCHATAGENTSTART;//坐席开始报告状态,用于绑定特定的坐席状态socketSession,focus中agent登录
+		 public String toString(){
+			 return super.toString().toLowerCase();
+		 }
+	}
+	/**
+	 * 聊天状态
+	 */
+	public enum ChatStatusTypeEnum{
+		WAITING(0),//等待
+		CHATTING(1),//聊天中
+		END(2);//结束
+		private Integer status;
+		ChatStatusTypeEnum(Integer status){
+			this.status=status;
+		}
+		public void setStatus(Integer status){
+			this.status=status;
+		}
+		public Integer getStatus(){
+			return this.status;
+		}
+		public String toString(){
+			return super.toString().toLowerCase();
+		}
+	}
+	/**
+	 *渠道来源
+	 */
+	public enum SourceTypeEnum{
+		WEBIM,
+		WEIXIN,
+		WEIBO,
+		VIDEO,
+		PHONE,
+		EMAIL;
+		public String toString(){
+			return super.toString().toLowerCase();
+		}
+	}
+	/* public static void main(String[] args) {
+		String string = SourceTypeEnum.VIDEO.toString();
+		System.out.println(" string"+string);
+	}*/
+	
+}
+
--- vkefu/src/main/java/com/vkefu/webim/web/controller/agentstatus/AgentstatusController.java
+++ vkefu/src/main/java/com/vkefu/webim/web/controller/agentstatus/AgentstatusController.java
@@ -1,10 +1,13 @@
 package com.vkefu.webim.web.controller.agentstatus;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 /**
  * 坐席状态Controller
  * created by yxm 20180610 10:58:21
  */
+@RestController
+@RequestMapping("/vkefu/agentstatus")
 public class AgentstatusController {
-
 
 }
